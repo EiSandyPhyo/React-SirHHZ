5e50581a03e3682a725d37c17d5ce586f93a5cfe
Ei Sandy Phyo <68009652+EiSandyPhyo@users.noreply.github.com>
2024-08-29 02:55:49 +0630

    feat:sample layout

--- react-old/src/App.jsx
+++ react-old/src/App.jsx
@@ -1,3 +1,6 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 export default function App() {
   const title = "Product Categories";
   const categories = [
@@ -7,15 +10,22 @@ export default function App() {
     "women's clothing",
   ];
   return (
-    <div id="category-section" className="p-5">
-      <p className="text-2xl text-gray-500 mb-3">{title}</p>
-      <div className="">
-        {categories.map((category) => (
-          <button className="border border-black px-4 py-2 me-2" key={category}>
-            {category}
-          </button>
-        ))}
-      </div>
-    </div>
+    <>
+      <Header />
+      <section id="category-section" className="p-5">
+        <p className="text-2xl text-gray-500 mb-3">{title}</p>
+        <div className="">
+          {categories.map((category) => (
+            <button
+              className="border border-black px-4 py-2 me-2"
+              key={category}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 }
---
+++ react-old/src/components/Header.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='px-5'>Header</header>
+  )
+}
+
+export default Header
